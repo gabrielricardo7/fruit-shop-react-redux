9997306f6084d575dae8b56cb1c41836024f5ae4
Gabriel Ricardo <gabrielricardo7@gmail.com>
2022-06-29 11:53:44 -0300

    fix: change package.json and rename App to "Loja de Frutas"

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,16 @@ import Cart from "../../components/Cart";
 import { MyHeader } from "./style";
 
 const Header = () => {
-    return (
-        <MyHeader>
-            <h1>KenzieShop de Frutas<br /><small>by Gabriel Ricardo</small></h1>
-            <Cart />
-        </MyHeader>
-    )
-}
+  return (
+    <MyHeader>
+      <h1>
+        Loja de Frutas
+        <br />
+        <small>by Gabriel Ricardo</small>
+      </h1>
+      <Cart />
+    </MyHeader>
+  );
+};
 
 export default Header;
